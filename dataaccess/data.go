cb5992b2b0dcc7b12c64bfd108955a5f9fc6bb16
HAJIME Fukuna <f97one@gmail.com>
2020-02-01 15:11:27 +0000

    DBインスタンスを保存できていなかった件を修正。

--- dataaccess/data.go
+++ dataaccess/data.go
@@ -6,20 +6,25 @@ import (
 	"github.com/rubenv/sql-migrate"
 )
 
-var Db *sqlx.DB
+var db *sqlx.DB
 
 const dialect string = "sqlite3"
 
 func init() {
-	Db, err := sqlx.Open(dialect, "file:airconcon.sqlite?_mode=rw&_journal=WAL&_auto_vacuu=incremental")
+	initDatabase()
+}
+
+func initDatabase() {
+	database, err := sqlx.Open(dialect, "file:airconcon.sqlite?_mode=rw&_journal=WAL&_auto_vacuu=incremental")
 	if err != nil {
 		panic(err)
 	}
 
 	// マイグレーションを自動実行
 	migration := &migrate.FileMigrationSource{Dir: "migrations/sqlite3"}
-	_, err = migrate.Exec(Db.DB, dialect, migration, migrate.Up)
+	_, err = migrate.Exec(database.DB, dialect, migration, migrate.Up)
 	if err != nil {
 		panic(err)
 	}
+	db = database
 }
