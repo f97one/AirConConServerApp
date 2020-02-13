c28ce1c30a3e62639043818f79cdba89417b299f
HAJIME Fukuna <f97one@gmail.com>
2020-02-13 12:54:43 +0000

    On/Offをboolに紐づける処理を追加。

--- utils/utils.go
+++ utils/utils.go
@@ -9,3 +9,23 @@ func BoolToInt(b bool) int {
 
 	return ret
 }
+
+// 引数の bool が true のとき「on」、そうでないとき「off」を返す。
+func BoolToOnOff(b bool) string {
+	ret := "off"
+	if b {
+		ret = "on"
+	}
+
+	return ret
+}
+
+// 引数が on のとき true を、そうでないとき false を返す。
+func OnOffToBool(onOff string) bool {
+	return onOff == "on"
+}
+
+// 引数が 1 のとき true を、そうでないとき false を返す。
+func IntToBool(i int) bool {
+	return i == 1
+}
